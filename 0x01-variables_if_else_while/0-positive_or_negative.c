7651bbbd9886fe0f342e089340953d37edf7070f
chima <chimac319@gmail.com>
2023-06-19 14:54:07 -0700

    update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <time.h>
 /**
  * main - generate random numbers to detect -ve or +ve
@@ -9,19 +10,21 @@ int main(void)
 {
 	int n;
 
-	srand(time(0);
+	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
-	(
-	       printf("%d is positive\n", n);
-	)		       
+	{
+		printf("%d is positive\n", n);
+	}
 	else if (n == 0)
-	(
-	        printf("%d is zero\n", n);
-	)	
+	{
+		printf("%d is zero\n", n);
+	}
 	else
-	(
-	        printf("%d is negative\n", n);
-	)
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
-}	
+}
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,22 +9,22 @@ int main(void)
 {
 	int n;
 	int lastd;
-	
+
 	strand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastd = n % 10;
-	if (lastd > 5)
-	(
- 	        print("last digit of %d is %d and is greater than s\n", n, lastd);
-	)
-	else if (lastd ==0)
-	(
-	        printf("last digit of %d is %d and is 0\n", n, lastd);
-	)	
-	else 
-	(
-	        printf("last digit of %d is %d and is less than 6and not o\n", n, lastd;)
-	)	
-	return (0);	
 
+	if (lastd > 5)
+	{
+		print("last digit of %d is %d and is greater than s\n", n, lastd);
+	}
+	else if (lastd == 0)
+	{
+		printf("last digit of %d is %d and is 0\n", n, lastd);
+	}
+	else
+	{
+		printf("last digit of %d is %d and is less than 6and not o\n", n, lastd;)
+	}
+	return (0);
 }
