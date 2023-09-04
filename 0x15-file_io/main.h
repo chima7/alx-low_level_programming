cc14c334b8ce594e33ffcafc5ff847a8f3deda80
chima7 <chimac2580@yahoo.com>
2023-09-04 14:30:47 -0700

    save file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,11 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int main(int ac, char **av);
